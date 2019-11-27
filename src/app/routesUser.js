a85c0abe1758a1c383de870b65cb30f93eda9e41
ismael <ismael@kioru.com>
2019-11-26 20:29:07 -0600

    Profile Fixed and logout if working now

--- src/app/routesUser.js
+++ src/app/routesUser.js
@@ -1,104 +1,101 @@
-module.exports = (app,passport)=>{
+module.exports = (app, passport) => {
+  const { Router } = require("express");
+  const router = Router();
+  const Image = require("./models/files");
+  const { unlink } = require("fs-extra");
+  const path = require("path");
 
-const {Router} = require('express');
-const router = Router();
-const Image = require('./models/files');
-const {unlink} = require('fs-extra');
-const path = require('path'); 
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-    app.get("/", (req, res) => {
-        res.render('index');
+  app.get("/signup", (req, res) => {
+    res.render("signup", {
+      message: req.flash("SignUpMessage")
     });
+  });
 
-    app.get("/signup", (req, res) => {
-        res.render('signup',{
-            message : req.flash('SignUpMessage')
-        })
+  app.get("/login", (req, res) => {
+    res.render("login", {
+      message: req.flash("LoginMessage")
     });
+  });
 
-    app.get("/login", (req, res) => {
-        res.render('login',{
-            message : req.flash('LoginMessage')
-        });
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/catalogo",
+      failureRedirect: "/login",
+      failureFlash: true
+    })
+  );
+
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/catalogo",
+      failureRedirect: "/signup",
+      failureFlash: true
+    })
+  );
+
+  app.get("/profile", isLogged, (req, res) => {
+    res.render("profile", {
+      user: req.user
     });
+  });
 
-    app.post("/login", passport.authenticate('local-login',{
-        successRedirect :'/catalogo',
-        failureRedirect:'/login',
-        failureFlash:true
-    }));
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
-    app.post("/signup",passport.authenticate('local-signup',{
-        successRedirect :'/catalogo',
-        failureRedirect:'/signup',
-        failureFlash:true
-    }));
-
-    app.get("/profile",isLogged, (req, res) => {
-        res.render('profile',{
-            user:req.user
-        });
-    });
-
-    app.get("/logout", (req, res) => {
-        req.logout();
-        res.redirect('/');
-    });
-
-
-    function isLogged(req,res,next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        return res.redirect('/');
+  function isLogged(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
+    return res.redirect("/");
+  }
 
+  /***
+   *
+   * This Section is about Micro services Files
+   *
+   */
 
-
-    /***
-     * 
-     * This Section is about Micro services Files
-     * 
-     */
-
-
-app.get("/catalogo",isLogged,async (req, res) => {
+  app.get("/catalogo", isLogged, async (req, res) => {
     const images = await Image.find();
-    console.log(images);
-    res.render('catalog',{images});
-});
+    res.render("catalog", { images });
+  });
 
-app.get("/upload",isLogged ,(req, res) => {
-    res.render('upload');
-});
+  app.get("/upload", isLogged, (req, res) => {
+    res.render("upload");
+  });
 
-app.post("/upload",isLogged,async(req,res)=>{
+  app.post("/upload", isLogged, async (req, res) => {
     const image = new Image();
     image.title = req.body.title;
     image.description = req.body.description;
     image.filename = req.file.filename;
-    image.path = '/files/uploads/' + req.file.filename;
+    image.path = "/files/uploads/" + req.file.filename;
     image.originalname = req.file.originalname;
     image.mimetype = req.file.mimetype;
     image.size = req.file.size;
-    
+
     await image.save();
-    res.redirect('/catalogo');
-});
+    res.redirect("/catalogo");
+  });
 
-app.get("/image/:id",isLogged,async (req, res) => {
-    const {id} = req.params; 
-    const image= await Image.findById(id);
-    res.render('profile',{image});
-});
+  app.get("/image/:id", isLogged, async (req, res) => {
+    const { id } = req.params;
+    const image = await Image.findById(id);
+    res.render("profileFile", { image });
+  });
 
-
-app.get("/image/:id/delete/",isLogged,async (req, res) => {
-    const {id} = req.params;
+  app.get("/image/:id/delete/", isLogged, async (req, res) => {
+    const { id } = req.params;
     const image = await Image.findByIdAndDelete(id);
-    await unlink(path.resolve('./src/public' + image.path))
-    res.redirect('/')
-});
-
-
-}
+    await unlink(path.resolve("./src/public" + image.path));
+    res.redirect("/catalogo");
+  });
+};
